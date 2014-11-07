48770eac079ac52ed9003d9b915024488907ee91
zhonghe987 <zhonghe987@163.com>
2014-11-07 10:28:56 +0800

    update code

--- webSpider/spiders/whereSpider.py
+++ webSpider/spiders/whereSpider.py
@@ -9,7 +9,7 @@ from scrapy.selector import Selector
 from scrapy.http import Request
 import  json
 import  urlparse
-
+from scrapy import log
 
 
 class whereSpider(CrawlSpider):
@@ -24,7 +24,7 @@ class whereSpider(CrawlSpider):
     #http://piao.qunar.com/ticket/list.json?keyword=上海&from=mps_remdd&total=790&page=1
     #def start_requests(self):
     #    yield Request(url =u"http://piao.qunar.com/ticket/list.json?keyword=上海&from=mps_remdd&page=1",callback = self.barse )
-        
+    {"score":"0.0","tuan":false,"sightId":1959588963,"intro":"公园闹中取静，让人感觉置身在大自然中。","free":false,"point":"121.475422,31.228764","address":"上海市卢湾区成都南路长乐路路口","foreign":false,"sightName":"广场公园","districts":"上海.上海.卢湾区","childrenCount":0,"saleCount":0,"qunarPrice":0,"marketPrice":0,"sightImgURL":"http://img1.qunarzz.com/sight/p0/201301/18/c943ae347883456693835fbb.jpg_280x200_6bcab18e.jpg","parentSightId":0,"hasPromotion":false,"recommend":false,"recommendLevel":"0.0"}
         
 
     def parse(self,response):
@@ -59,13 +59,13 @@ class whereSpider(CrawlSpider):
           
         
     def inner_page(self,response):
+        log.msg(response.url)
         res = Selector(response)
         item = WebspiderItem()
         if not res.xpath('//h1[@class="sight_info_name"]/@title'):
             yield Request(url=response.url, dont_filter=True)
         item['detil_title'] = res.xpath('//h1[@class="sight_info_name"]/@title').extract()[0]
         title_ticket = res.xpath('//h3[@class="ticket_item_title ticket_item_title_mainpage"]/text()').extract()
-            
         price = res.xpath('//em[@class="txt_orange"]/strong[not(@style)]/text()').extract()
         if title_ticket and price:
             item['ticket_title'] = ','.join(['--'.join(k) for k in zip(title_ticket,price)])
